74c7df92c35b03488c7a1bd4611c6596463aadeb
Aadith PM <aadithpm@gmail.com>
2020-12-05 23:26:49 -0600

    Added Day 4 part 2

--- Day4/answer.py
+++ Day4/answer.py
@@ -1,4 +1,5 @@
-from typing import List, Mapping
+import re
+from typing import List, Mapping, Callable
 
 input_file = "input.txt"
 
@@ -18,22 +19,29 @@ def parse_passports(filepath: str) -> List[str]:
             for field in line_data:
                 key, value = field.split(':')
                 passport[key] = value
+    passports.append(passport)
     return passports
 
-def is_valid_passport(passport: Mapping[str, str], fields: List[str]) -> bool:
-    """Check if passport contains all given fields
+def is_valid_passport(passport: Mapping[str, str], fields: Mapping[str, str]) -> bool:
+    """Check if passport contains all given fields and field patterns
 
     :param passport: mapping of field name -> value
-    :param fields: list of fields to be checked
-    :return: true if all fields are present in passport
+    :param fields: list of fields to be checked with regex patterns
+    :return: true if all fields are present in passport and values match patterns
     """
-    return all(key in passport.keys() for key in fields)
+    if all(key in passport.keys() for key in fields.keys()):
+        for attribute, value in passport.items():
+            if attribute in fields and not fields[attribute](value):
+                return False
+        print(passport)
+        return True
+    return False    
 
-def count_valid_passports(passports: List[Mapping[str, str]], fields: List[str]) -> int:
+def count_valid_passports(passports: List[Mapping[str, str]], fields: Mapping[str, str]) -> int:
     """Counts number of valid passports in given passport list
 
     :param passports: list of passport mappings - field name -> value
-    :param fields: list of fields to be checked
+    :param fields: list of fields to be checked with patterns
     :return: number of valid passports in list
     """
     count = 0
@@ -42,14 +50,15 @@ def count_valid_passports(passports: List[Mapping[str, str]], fields: List[str])
     return count
 
 passports = parse_passports(input_file)
-fields = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-]
+
+fields = {
+    'byr': lambda x: int(x) >= 1920 and int(x) <= 2002,
+    'iyr': lambda x: int(x) >= 2010 and int(x) <= 2020,
+    'eyr': lambda x: int(x) >= 2020 and int(x) <= 2030,
+    'hgt': lambda x: re.match(r"(?:1(?:[5-8]\d|9[0-3])cm|(?:59|6\d|7[0-6])in)", x),
+    'hcl': lambda x: re.match(r"#[\da-f]{6}", x),
+    'ecl': lambda x: x in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'},
+    'pid': lambda x: re.match(r"\d{9}$", x),
+}
 
 print(count_valid_passports(passports, fields))
